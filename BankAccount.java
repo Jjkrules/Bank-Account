67dbb6bcef99210b633dea0a2cc1ff5b01534b0e
Jeswin James <tul47834@temple.edu>
2026-04-13 20:11:41 -0400

    Updated main function

--- BankAccount.java
+++ BankAccount.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class BankAccount {
 
     private double balance;  
@@ -16,4 +18,43 @@ public class BankAccount {
     public double getBalance() {
         return balance;
     }
+
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount(1000.0);
+        System.out.println("Welcome to the Bank!");
+
+        System.out.println("Your balance: " + account.getBalance());
+        Scanner scanner = new Scanner(System.in);
+        
+        boolean continueBanking = true;
+
+        while (continueBanking) {
+            System.out.println("What would you like to do?" );
+                System.out.println("1. Deposit");
+                System.out.println("2. Withdraw");
+                System.out.println("3. Check Balance");
+                System.out.println("4. Exit");
+
+                int userInput = scanner.nextInt(); 
+                
+            switch(userInput){
+                case 1: 
+                    System.out.println("How much are you depositing?");
+                    double money = scanner.nextDouble(); 
+                    account.deposit(money);
+                case 4:
+                    System.out.println("Thank you for banking with us!");
+                    continueBanking = false;
+            }
+             
+        }
+        /* 
+        account.deposit(500.0);
+        System.out.println("Balance after deposit: " + account.getBalance());
+        
+        account.withdraw(200.0);
+        System.out.println("Balance after withdrawal: " + account.getBalance());
+        */
+    }
+     
 }
